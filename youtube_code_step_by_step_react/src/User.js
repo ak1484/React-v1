574bf0b7d383c374ee6cbfc8389082f0c2f25cb1
ak1484 <ankitsaharan1484@gmail.com>
2021-05-13 22:32:40 +0530

    click Event and Function

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,8 +1,20 @@
-// Class component
-import React from "react";
-class User extends React.Component {
-  render() {
-    return <h1> User Class component</h1>;
+//click Event and Function
+//never use apple() inside curly braces in function comp
+//{=>alert("apple fun")} we can also use arrow fun
+function User() {
+  let data = "ankit";
+  function apple() {
+    data = "viki";
+    //thats why we use state and props
+    alert("apple function call");
   }
+  return (
+    <>
+      <h1>click Event and Function</h1>
+      <h2>{data}</h2>
+      {/* we can't update data directly using variable */}
+      <button onclick={apple}>Click me</button>
+    </>
+  );
 }
 export default User;
