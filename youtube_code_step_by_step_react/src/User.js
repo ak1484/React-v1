6da00bc1e8d2d7902d7d5af91be88fd63d76deb0
ak1484 <ankitsaharan1484@gmail.com>
2021-05-14 10:50:32 +0530

    Get input box value and print

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,23 +1,20 @@
-//props in class component with on click  state change
-import React from "react";
-import Student from "./Student";
-class User extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "ankit",
-      email: "ankit@test.com",
-    };
+//Get input box value and print 
+import React, { useState } from "react";
+function User() {
+  const [data, setData] = useState("");
+  const [print, setPrint] = useState(null);
+  function getData(val) {
+    console.log(val.target.value);
+    setData(val.target.value);
+    setPrint(false)
   }
-  render() {
-    return (
-      <>
-        <h1>props in class component</h1>
-        {/* this why we transfer props in c com */}
-        <Student name={this.state.name} email={this.state.email} />
-        <button onClick={() =>this.setState({name:"vikram",email:"vikram@text.com"})}>Update Name and Email</button>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Get input box value</h1>
+      {print ? <h2>{data}</h2> : null}
+      <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>Print Data</button>
+    </>
+  );
 }
 export default User;
