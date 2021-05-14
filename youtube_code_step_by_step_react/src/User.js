7ba4415e2349b3913e51e5da3f9b54d0bbd45165
ak1484 <ankitsaharan1484@gmail.com>
2021-05-14 21:34:50 +0530

    useEffect hook

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,12 +1,17 @@
-//Hooks in React js
-import React, { useState} from "react";
+//useEffect hook
+import React, { useState,useEffect} from "react";
 function User() {
-  const [data,setData]=useState("ankit")
+  const [count,setCount]=useState(0)
+  useEffect(()=>{
+    console.warn("use Effect call")
+    //component did mount k wajas se call ho jata hai
+  },[count])
+  //count wala ki wajas se chal rha hai use Effect 
   return (
     <div>
-      <h1>Hooks in React js</h1>
-      <h2>{data}</h2>
-      <button onClick={()=>setData("vikram")}>Update Data</button>
+      <h1>useEffect hook</h1>
+      <h2>{count}</h2>
+      <button onClick={()=>setCount(count+1)}>Update Data</button>
     </div>
   );
 }
