251a29feaa1e8f3bf3cd589aff9eee6e389471a7
ak1484 <ankitsaharan1484@gmail.com>
2021-05-14 09:50:46 +0530

    state in class component

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,21 +1,27 @@
-// state in functional component
-import React,{useState} from 'react';
-function User() {
-  const [data,setData]=useState("ankit")
-  // let data = "ankit";
-  // function updateData() {
-  //   data = "viki";
-  //   alert(data);
-  // }
-  function updateData(){
-    setData("vikram")
+// state in class component
+import React from 'react';
+class User extends React.Component {
+  constructor()
+  {
+    //super javasript ki property hai
+    super()
+    this.state={
+      // data:"ankit"
+      count:0
+    }
   }
+  updateData(){
+    // alert("update data")
+    // this.setState({data:"vikram"})
+    this.setState({count:this.state.count+1})
+  }
+  render(){
   return (
     <>
-      <h1>state in functional component</h1>
-      <h2>{data}</h2>
-      <button onClick={updateData}>Update data</button>
+      <h1>state in class component</h1>
+      <h2>{this.state.count}</h2>
+      <button onClick={()=>this.updateData()}>Update data</button>
     </>
-  );
+  );}
 }
 export default User;
