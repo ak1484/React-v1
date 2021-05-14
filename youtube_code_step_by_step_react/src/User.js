167eb3a8f037339744c50b928ea0776db2745c2d
ak1484 <ankitsaharan1484@gmail.com>
2021-05-14 11:16:09 +0530

    Basic Form filling

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,11 +1,38 @@
-//Hide and show element
-import React ,{useState} from 'react'
+//Basic Form filling
+
+import { useState } from "react";
 function User() {
-  const[status,setStatus]=useState(true)
+  const [name, setName] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [tnc, setTnc] = useState(false);
+  function getFormData(e) {
+    console.warn(name, interest, tnc);
+    e.preventDefault();
+  }
   return (
     <>
-      {status?<h1>Hide and show element</h1>:null}
-      <button onClick={()=>setStatus(!status)}>Toggle</button>
+      <h1>Basic Form filling</h1>
+      <form onSubmit={getFormData}>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <br />
+        <br />
+        <select onChange={(e) => setInterest(e.target.value)}>
+          <option>Select Options</option>
+          <option>Marvel</option>
+          <option>DC</option>
+        </select>
+        <br />
+        <br />
+        <input type="checkbox" onChange={(e) => setTnc(e.target.checked)} />
+        <span>Accept terms and conditions</span>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
