17ac157c425bed388d99c44beb1b5fd882718d8c
ak1484 <ankitsaharan1484@gmail.com>
2021-05-14 10:57:48 +0530

    Hide and show element

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,19 +1,11 @@
-//Get input box value and print 
-import React, { useState } from "react";
+//Hide and show element
+import React ,{useState} from 'react'
 function User() {
-  const [data, setData] = useState("");
-  const [print, setPrint] = useState(null);
-  function getData(val) {
-    console.log(val.target.value);
-    setData(val.target.value);
-    setPrint(false)
-  }
+  const[status,setStatus]=useState(true)
   return (
     <>
-      <h1>Get input box value</h1>
-      {print ? <h2>{data}</h2> : null}
-      <input type="text" onChange={getData} />
-      <button onClick={() => setPrint(true)}>Print Data</button>
+      {status?<h1>Hide and show element</h1>:null}
+      <button onClick={()=>setStatus(!status)}>Toggle</button>
     </>
   );
 }
