625f46c58b1c55922c2721984dfc4444d5ba2d19
ak1484 <ankitsaharan1484@gmail.com>
2021-05-15 22:19:01 +0530

    Bootstrap

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,17 +1,10 @@
-//Styles in React js
-//inline css,normal css file with .css estension and 3rd modular css
-import "./styles/style.css";
-import style from "./styles/custom.module.css";
+//Bootstrap
+import {Button} from 'react-bootstrap'
 function User() {
   return (
     <div>
-      <h1>Styles in React js</h1>
-      <h1 style={{ color: "red", backgroundColor: "blue", margin: 20 }}>
-        InLine Styles
-      </h1>
-      <h1 className="primary">normal css style</h1>
-      <h1 className={style.sucess}>Modular css style</h1>
-      <h1>styled component not yet done</h1>
+      <h1>Bootstrap</h1>
+      <Button onClick={()=>alert("bootstrap")} >Click me</Button>
     </div>
   );
 }
