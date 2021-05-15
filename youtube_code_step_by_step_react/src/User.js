60ca35f780c1073c63a8a56311bbe4b4b2c74c9b
ak1484 <ankitsaharan1484@gmail.com>
2021-05-15 13:23:01 +0530

    Styles in React js

--- youtube_code_step_by_step_react/src/User.js
+++ youtube_code_step_by_step_react/src/User.js
@@ -1,15 +1,17 @@
-//useEffect hook
-import React, { useState} from "react";
-import Student from "./Student";
+//Styles in React js
+//inline css,normal css file with .css estension and 3rd modular css
+import "./styles/style.css";
+import style from "./styles/custom.module.css";
 function User() {
-  const [count,setCount]=useState(0)
-  const [data,setData]=useState(10)
   return (
     <div>
-      <h1>useEffect hook</h1>
-      <Student data={data} count={count}/>
-      <button onClick={()=>setCount(count+1)}>Update Count</button>
-      <button onClick={()=>setData(data+1)}>Update Data</button>
+      <h1>Styles in React js</h1>
+      <h1 style={{ color: "red", backgroundColor: "blue", margin: 20 }}>
+        InLine Styles
+      </h1>
+      <h1 className="primary">normal css style</h1>
+      <h1 className={style.sucess}>Modular css style</h1>
+      <h1>styled component not yet done</h1>
     </div>
   );
 }
